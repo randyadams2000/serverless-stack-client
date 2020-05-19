fffa66490255fc5ba27cadc7cac2ec912ac8ebc8
Randy Adams <randy.adams@avinew.com>
2020-05-19 16:08:09 -0700

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,18 @@
+
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "immortify-uploads"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://49lipoe04b.execute-api.us-east-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_vwBP95FkR",
+    APP_CLIENT_ID: "248m7ijfe0qs0vv81kap2gb2ls",
+    IDENTITY_POOL_ID: "us-east-1:360bb111-2a86-4362-a4fb-e5edefef536a"
+  }
+};
+
