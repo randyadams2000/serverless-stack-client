315e69b9752967393d346e7735341260e18bd5dd
Randy Adams <randy.adams@avinew.com>
2020-05-19 17:02:39 -0700

    Setting up our React app

--- src/Routes.js
+++ src/Routes.js
@@ -2,6 +2,9 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
 import NotFound from "./containers/NotFound";
+import Login from "./containers/Login";
+import Signup from "./containers/Signup";
+import NewNote from "./containers/NewNote";
 
 export default function Routes() {
   return (
@@ -9,6 +12,16 @@ export default function Routes() {
       <Route exact path="/">
         <Home />
       </Route>
+      <Route exact path="/login">
+  		<Login />
+	  </Route>
+	  <Route exact path="/signup">
+      <Signup />
+      </Route>
+      <Route exact path="/notes/new">
+      <NewNote />
+     </Route>
+
       {/* Finally, catch all unmatched routes */}
 	<Route>
 	  <NotFound />
