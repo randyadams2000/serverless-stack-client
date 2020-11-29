804139abcc4f1d5880b19fbe226f50c39232f2d8
Randy Adams <randy.adams@avinew.com>
2020-11-29 10:14:19 -0800

    Development Change

--- src/containers/Record.js
+++ src/containers/Record.js
@@ -1,44 +1,28 @@
 import React, {} from "react";
-//import ScriptTag from 'react-script-tag';
 import {  } from "react-router-dom";
-import {Helmet} from "react-helmet";
+import { useRecordWebcam } from 'react-record-webcam'
+//import {Helmet} from "react-helmet";
 //import { API, Storage } from "aws-amplify";
 //import { onError } from "../libs/errorLib";
 //import { FormGroup, FormControl, ControlLabel } from "react-bootstrap";
 //import LoaderButton from "../components/LoaderButton";
-import { PageHeader } from "react-bootstrap";
+//import { PageHeader } from "react-bootstrap";
 import "./Record.css";
 
 //import { s3Upload } from "../libs/awsLib";
 
-export default function Record() {
-
-
-
-
+export default function Record(props) {
+    const recordWebcam = useRecordWebcam();
 return (
-  <div><Helmet>
-  <script src="./recordRTC.js" type="text/javascript" />
-  <script src="./main.js" type="text/javascript" />
-  <script src="https://sdk.amazonaws.com/js/aws-sdk-2.1.12.min.js" type="text/javascript" />
-  </Helmet>
+    <div>
+    <p>Camera status: {recordWebcam.status}</p>
+    <button onClick={recordWebcam.open}>Open camera</button>
+    <button onClick={recordWebcam.start}>Start recording</button>
+    <button onClick={recordWebcam.stop}>Stop recording</button>
+    <button onClick={recordWebcam.retake}>Retake recording</button>
+    <button onClick={recordWebcam.download}>Download recording</button>
+    <video ref={recordWebcam.webcamRef} autoPlay muted />
+    <video ref={recordWebcam.previewRef} autoPlay muted loop />
   </div>
-   <div className="Notes">
-  <PageHeader>Record</PageHeader>
-  <button id="btn-start-recording">
-      Start Recording
-  </button>
-  <button disabled="disabled" id="btn-stop-recording">
-      Stop Recording
-  </button>
-  <button id="cancel-button">
-      Cancel Upload
-  </button>
-  <hr/>
-  <video autoplay="" controls="" id="my-preview">
-  </video>
-  <p id="percentage">
-  </p>
-</div>
 );
 }
